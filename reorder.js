e05017c697d48dc0fa87653c08dcad63e8ced1bc
Santosl2c <mfilype2019@gmail.com>
2022-06-19 16:17:15 -0300

    feat: Project finished

---
+++ reorder.js
@@ -0,0 +1,107 @@
+const ARRAY_LIMIT = 5;
+
+const list = document.querySelector("ul");
+const orderIndicator = document.getElementById("order");
+
+let itemDragging = null;
+
+const data = Array.from(Array(ARRAY_LIMIT).keys());
+
+function randomData() {
+  return data
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort);
+}
+
+function createElements() {
+  list.innerHTML = "";
+
+  randomData()
+    .map((a) => a.value)
+    .forEach((item) => {
+      const li = document.createElement("li");
+
+      li.innerHTML = item + 1;
+      li.setAttribute("draggable", true);
+      addEventListeners(li);
+
+      list.appendChild(li);
+    });
+
+  checkOrder();
+}
+
+function addEventListeners(item) {
+  if (!item) return;
+
+  item.addEventListener("drag", onDrag);
+  item.addEventListener("dragover", onDragOver);
+  item.addEventListener("dragenter", onDragEnter);
+  item.addEventListener("drop", onDrop);
+  item.addEventListener("dragleave", onDragLeave);
+  item.addEventListener("dragend", (e) => {
+    e.target.classList.remove("dragging");
+  });
+
+  return item;
+}
+
+function onDrag(e) {
+  itemDragging = this;
+
+  this.classList.add("dragging");
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+}
+
+function onDrop(e) {
+  e.stopPropagation();
+  const dragItemText = itemDragging.innerHTML;
+  this.classList.remove("dragEnter");
+
+  itemDragging.innerHTML = this.innerHTML;
+  this.innerHTML = dragItemText;
+
+  itemDragging.classList.remove("dragging");
+
+  checkOrder();
+
+  itemDragging = null;
+}
+
+function onDragLeave() {
+  this.classList.remove("dragEnter");
+}
+
+function onDragEnter(e) {
+  this.classList.add("dragEnter");
+}
+
+function wrongOrCorrect(type = "wrong") {
+  if (type === "correct") {
+    orderIndicator.classList.remove("wrong");
+    orderIndicator.classList.add("correct");
+    orderIndicator.innerText = "Correct! Good Job";
+  } else {
+    orderIndicator.classList.add("wrong");
+    orderIndicator.classList.remove("correct");
+    orderIndicator.innerText = "Wrong";
+  }
+}
+
+function checkOrder() {
+  list.querySelectorAll("li").forEach((item, index) => {
+    const dataToString = String(data[index] + 1);
+
+    if (dataToString !== item.innerText) {
+      wrongOrCorrect();
+      return;
+    }
+
+    wrongOrCorrect("correct");
+  });
+}
+
+createElements();
